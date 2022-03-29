3d5d5bc45f2bb78466f62ec676c59f3edfd2f39c
nathans <nathans@carallon.com>
2022-03-29 18:27:52 +0100

    Gather more analysis data

--- src/bin/analyse_solvers.rs
+++ src/bin/analyse_solvers.rs
@@ -6,4 +6,7 @@ fn main() {
     println!("Analysing random valid word");
     let analysis = analyse::<RandomValidWord>(&wordle::WORD_LENGTH, &words::Variant::Uk, wordle::GUESSES);
     println!("{:.2}% solved", analysis.percent_solved);
+    println!("Mean: {:.2}", analysis.mean);
+    println!("Median: {}", analysis.median);
+    println!("Range: {} - {}", analysis.min, analysis.max);
 }
--- src/solver/analyser.rs
+++ src/solver/analyser.rs
@@ -3,7 +3,11 @@ use crate::{words, game::{self, GuessResult}};
 use super::{Solver, solver::Guess};
 
 pub struct Analysis {
-    pub percent_solved: f32
+    pub percent_solved: f32,
+    pub mean: f32,
+    pub median: i8,
+    pub min: i8,
+    pub max: i8
 }
 
 pub fn analyse<S: Solver>(word_length: &i8, variant: &words::Variant, guesses: i8) -> Analysis {
@@ -24,6 +28,18 @@ pub fn analyse<S: Solver>(word_length: &i8, variant: &words::Variant, guesses: i
     };
 
     println!("Analysing {} words", words.len());
-    let successes = words.iter().map(|word| solve_for(word)).filter(is_success).count();
-    Analysis { percent_solved: ((successes as f32 / words.len() as f32) * 100 as f32) }
+    let successes: Vec<Vec<Guess>> = words.iter().map(|word| solve_for(word)).filter(is_success).collect();
+    let success_count = successes.len();
+    
+    let percent_solved = (success_count as f32 / words.len() as f32) * 100 as f32;
+    let mut guess_counts: Vec<i8> = successes.iter().map(|guesses| guesses.len() as i8).collect();
+    guess_counts.sort();
+
+    let mean = guess_counts.iter().map(|i| *i as f64).sum::<f64>() / success_count as f64;
+    let median = guess_counts.iter().nth(success_count / 2).unwrap();
+    let min = guess_counts.iter().next().unwrap();
+    let max = guess_counts.iter().last().unwrap();
+
+
+    Analysis{ percent_solved: percent_solved, mean: mean as f32, median: *median, min: *min, max: *max}
 }
