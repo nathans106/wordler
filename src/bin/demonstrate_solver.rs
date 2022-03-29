e90f7bfc7b9a435c10ea238b21919558fc2e9612
nathans <nathans@carallon.com>
2022-03-29 17:31:04 +0100

    Improve demonstrate_solver printing

--- src/bin/demonstrate_solver.rs
+++ src/bin/demonstrate_solver.rs
@@ -5,9 +5,9 @@ fn main() {
     let guesses = RandomValidWord::solve(&mut game);
     guesses.iter().for_each(|guess| println!("{}", guess.word));
     match &guesses.iter().last().unwrap().result {
-        GuessResult::Correct => println!("Success!"),
+        GuessResult::Correct => println!("Success! Took {} guesses", guesses.len()),
         GuessResult::Incorrect(_) => panic!(),
         GuessResult::NotInDictionary => panic!(),
-        GuessResult::GameOver(data) => println!("{}", data.word),
+        GuessResult::GameOver(data) => println!("Failed to guess {}", data.word),
     }
 }
