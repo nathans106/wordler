aba796d8cfd49937548f00640afd46106d0b7de3
nathans <nathans@carallon.com>
2022-03-29 14:24:07 +0100

    Move words code to words.rs
    Makes it clearer to see which the relevant file in the IDE tabs.

---
+++ src/words/words.rs
@@ -0,0 +1,31 @@
+static UK_STRS: &str = include_str!("google-10000-english\\google-10000-english.txt");
+static USA_STRS: &str = include_str!("google-10000-english\\google-10000-english-usa.txt");
+
+#[derive(Clone, Copy)]
+pub enum Variant {
+    Uk,
+    Usa
+}
+
+pub type Words = Vec<&'static str>;
+
+pub fn with_length(length: &i8, variant: &Variant) -> Words {
+    let strs = match variant {
+        Variant::Uk => UK_STRS,
+        Variant::Usa => USA_STRS
+    };
+
+    strs.lines().filter(|line| line.len() == (*length as usize)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fives(){
+        let words = with_length(&5, &Variant::Uk);
+        assert!(words.len() > 0);
+        words.iter().for_each(|word| assert!(word.len() == 5));
+    }
+}
